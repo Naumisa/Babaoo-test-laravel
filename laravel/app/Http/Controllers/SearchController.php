74cfe4997382173cd76bf7ff2fc0d443146487df
twagner@antropus.fr <twagner@antropus.fr>
2023-12-16 16:08:39 +0100

    Mise en place du cache des recherches

--- laravel/app/Http/Controllers/SearchController.php
+++ laravel/app/Http/Controllers/SearchController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Search;
+use App\Models\SearchCache;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
 
@@ -17,17 +18,33 @@ class SearchController extends Controller
         $search->user_id = auth()->id();
         $search->save();
 
-        // Effectuer la requête à l'API
-        $response = Http::get('https://dummyjson.com/products/search', [
-            'q' => $searchTerm
-        ]);
+        $cachedSearch = SearchCache::where('query', $searchTerm)->first();
 
-        // Vérifier si la requête est réussie
-        if ($response->successful()) {
-            $results = $response->json()['products'];
-        } else {
-            $results = [];
+        if ($cachedSearch)
+        {
+            $results = json_decode($cachedSearch->results, true);
         }
+        else
+        {
+            // Effectuer la requête à l'API
+            $response = Http::get('https://dummyjson.com/products/search', [
+                'q' => $searchTerm
+            ]);
+
+            $results = [];
+
+            // Vérifier si la requête est réussie
+            if ($response->successful())
+            {
+                $results = $response->json()['products'];
+            }
+
+            SearchCache::create([
+                'query' => $searchTerm,
+                'results' => json_encode($results)
+            ]);
+        }
+
         return view('pages.search', compact('searchTerm', 'results'));
     }
 }
